1001ed78438f3b301acfa3aee36772980ecd94fd
Atsushi <>
2019-05-15 00:17:54 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  mode: 'production',
+  entry: `${__dirname}/src/index.ts`,
+  output: {
+    path: `${__dirname}/docs`,
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: ['raw-loader', 'glslify-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [require('autoprefixer')({ grid: true })]
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpg|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [],
+  performance: {
+    hints: false
+  },
+  devServer: {
+    contentBase: `${__dirname}/docs`,
+    port: 3000,
+    hot: true,
+    open: true
+  }
+};
